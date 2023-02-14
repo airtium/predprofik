5f211bd1a80cc8cc1251845c8697b9805b8b9425
airtium <artem.1.rubtsov@gmail.com>
2023-02-14 21:22:50 +0300

    поправка

--- server/app.py
+++ server/app.py
@@ -397,7 +397,7 @@ def productScanForm():
     <!doctype html>
     <title>Загрузить новый файл</title>
     <h1>Загрузить новый файл</h1>
-    <form method=post enctype=multipart/form-data action='/product/scan'>
+    <form method=post enctype=multipart/form-data action='/product/scantest'>
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
@@ -488,6 +488,63 @@ def productScan():
         print("Некорректный запрос, должен быть POST")
         return 500
 
+#Функция для проверки нейронки
+@app.route('/product/scantest', methods=['GET', 'POST'])
+def productScanTest():
+    if request.method == 'POST':
+        print("Получили запрос на сканирование")
+
+        request_files = request.files
+        if not request_files:
+            print('Нет файлов в запросе')
+            return 500
+        try:
+            request_file = request.files['file']
+        except:
+            print('Не могу забрать файл из запроса')
+            return 500
+        # создаем временную директорию для файлов
+        if not os.path.exists(TMP_DIR+'/upload'):
+            os.makedirs(TMP_DIR+'/upload')
+        filePath = TMP_DIR+"/img.jpg"
+        # сохраняем файл
+        request_file.save(filePath)
+        print("Сохранили файл")
+
+        productMLName = scan(filePath)
+        #удаляем файл, чтобы не засорять сервер
+        os.remove(filePath)
+
+        #получаем название продукта
+        try:
+            conn = psycopg2.connect(database=DB_DATABASE,
+                                    host=DB_SERVER,
+                                    user=DB_USER,
+                                    password=DB_PASSWORD,
+                                    port=DB_PORT)
+        except:
+            print("Не могу установить соединение с базой данных")
+            return 500
+        cursor = conn.cursor()
+        query = ("SELECT name FROM product_names WHERE id_productname="+str(productMLName))
+        cursor.execute(query)
+        res = cursor.fetchone()
+        if res:
+            productName = res[0]
+            print("Название выбранного продукта: ",productName)
+        else:
+            print("Продукт с выбранным номером не найден в справочнике")
+            return 500
+
+        return """
+            <!doctype html>
+            <title>Распознавание ценника</title>
+            <h1>Распознавание ценника</h1>
+            <р>Нейронка распознала продукт, это:</p>
+            """+"<p>Номер продукта:"+str(productMLName)+", название продукта: "+productName+"</html>"
+    else:
+        print("Некорректный запрос, должен быть POST")
+        return 500
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
